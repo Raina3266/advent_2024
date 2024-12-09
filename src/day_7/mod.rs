df0926ac06a2acc8d061e8f54d1e8ad8425b656f
raina <chenganlin990326@gmail.com>
2024-12-09 19:26:02 +0000

    12.09

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -1,3 +1,5 @@
+use std::{collections::VecDeque, fmt::Binary};
+
 #[cfg(test)]
 const TEST_INPUT: &str = include_str!("./test_input.txt");
 pub const INPUT: &str = include_str!("./input.txt");
@@ -9,6 +11,12 @@ enum Operation {
 }
 
 impl Operation {
+    fn switch(&self) -> Option<Self> {
+        match self {
+            Self::Add => Some(Self::Mul),
+            Self::Mul => None,
+        }
+    }
     fn calculation(&self, x: i32, y: i32) -> i32{
         match self {
             Self::Add => x + y,
@@ -25,19 +33,40 @@ pub fn part_1(string: &str) -> i32 {
 }
 
 fn check_one_line(nums: Vec<i32>, result: i32) -> bool {
-    let mut operations: Vec<Operation> = vec![Operation::Mul; nums.len() - 1];
-    operations.iter().pe
-
+    let mut operations: VecDeque<Operation> = VecDeque::new();
+    let length = nums.len() - 1;
 }
 
-fn operations_combination(mut operations: Vec<Operation>) -> Vec<Vec<Operation>> {
-    let mut result: Vec<Vec<Operation>> = vec![];
-    for b in 0..=255 {
-        b.into()
+fn operations_combination(mut operations: VecDeque<Operation>, length: usize) -> Vec<Vec<Operation>> {
+    let mut result: Vec<Vec<i32>> = vec![];
+    operations.push_front(Operation::Add);
+
+    for i in 0..length{
+        if operations[i] == Operation::Add {
+            operations[i].switch();
+        } else {
+            operations[i].switch();
+            operations.push_front(Operation::Mul);
+        }
     }
+    println!("{:?}", result);
+    result
 }
 
 #[test]
-fn part_1_test() {
-    assert_eq!(part_1(TEST_INPUT), 41);
+fn feature() {
+
+    
 }
+
+
+#[test]
+fn part_1_test() {
+    for my_int in 0..10{
+        println!("{my_int:b}");
+    }
+    
+    assert_eq!(operations_combination(5), [[1,2],[2,3]]);
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day_4;
 mod day_5;
 mod day_6;
 mod day_7;
+mod day_8;
 
 fn main() {
     println!("{}", day_6::part_2(day_6::INPUT));
