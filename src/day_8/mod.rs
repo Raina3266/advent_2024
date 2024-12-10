4e9287279a98f09a2988733b822f61a14755cb5c
raina <chenganlin990326@gmail.com>
2024-12-10 17:48:13 +0000

    part 1

--- src/day_8/mod.rs
+++ src/day_8/mod.rs
@@ -4,7 +4,6 @@ use std::collections::{HashMap, HashSet};
 const TEST_INPUT: &str = include_str!("./test_input.txt");
 pub const INPUT: &str = include_str!("./input.txt");
 
-
 #[derive(PartialEq, Clone)]
 struct Grid {
     data: Vec<Vec<char>>,
@@ -41,7 +40,8 @@ impl Grid {
     }
 
     fn get(&self, location: (isize, isize)) -> Option<char> {
-        if location.0 < self.width && location.1 < self.height && location.0 >= 0 && location.1 >= 0 {
+        if location.0 < self.width && location.1 < self.height && location.0 >= 0 && location.1 >= 0
+        {
             Some(self.data[location.1 as usize][location.0 as usize])
         } else {
             None
@@ -51,16 +51,19 @@ impl Grid {
 
 pub fn part_1(string: &str) -> i32 {
     let grid = Grid::new(string);
-    let mut ans: HashSet<(isize, isize),_> = HashSet::new();
+    let mut ans: HashSet<(isize, isize), _> = HashSet::new();
 
     let mut antennas_map: HashMap<char, Vec<(isize, isize)>> = HashMap::new();
-    let antennas = get_antennas(&grid, '.');
+    let antennas = get_antennas(&grid);
     for antenna in &antennas {
-        antennas_map.entry(grid.get(*antenna).unwrap()).and_modify(|v| v.push(antenna.clone())).or_insert(vec![antenna.clone()]);
+        antennas_map
+            .entry(grid.get(*antenna).unwrap())
+            .and_modify(|v| v.push(*antenna))
+            .or_insert(vec![*antenna]);
     }
     for antenna in antennas_map {
         let pairs = pair_two_antennas(&antenna.1);
-        for pair in pairs{
+        for pair in pairs {
             let first_antenna = antenna.1[pair.0];
             let second_antenna = antenna.1[pair.1];
             let antinodes = put_antinodes(first_antenna, second_antenna, &grid);
@@ -74,11 +77,11 @@ pub fn part_1(string: &str) -> i32 {
     ans.len().try_into().unwrap()
 }
 
-fn get_antennas(grid: &Grid, letter: char) -> Vec<(isize, isize)> {
+fn get_antennas(grid: &Grid) -> Vec<(isize, isize)> {
     let mut result = vec![];
     for y in 0..grid.height {
         for x in 0..grid.width {
-            if grid.data[y as usize][x as usize] != letter {
+            if grid.data[y as usize][x as usize] != '.' {
                 result.push((x, y));
             }
         }
@@ -86,8 +89,11 @@ fn get_antennas(grid: &Grid, letter: char) -> Vec<(isize, isize)> {
     result
 }
 
-// (8,1), (5,2), (7,3), (4,4)
-fn put_antinodes(first_antenna: (isize, isize), second_antenna: (isize, isize), grid: &Grid) -> Vec<(isize, isize)> {
+fn put_antinodes(
+    first_antenna: (isize, isize),
+    second_antenna: (isize, isize),
+    grid: &Grid,
+) -> Vec<(isize, isize)> {
     let mut result: Vec<(isize, isize)> = vec![];
     let mut first_antinode: (isize, isize) = (0, 0);
     let mut second_antinode: (isize, isize) = (0, 0);
@@ -101,40 +107,46 @@ fn put_antinodes(first_antenna: (isize, isize), second_antenna: (isize, isize), 
         second_antinode.1 = second_antenna.1 - height_gap;
     } else if first_antenna.0 < second_antenna.0 && first_antenna.1 >= second_antenna.1 {
         first_antinode.0 = first_antenna.0 - width_gap;
-        first_antinode.1 = first_antenna.0 + width_gap;
+        first_antinode.1 = first_antenna.1 + height_gap;
 
         second_antinode.0 = second_antenna.0 + width_gap;
         second_antinode.1 = second_antenna.1 - height_gap;
     } else if first_antenna.0 >= second_antenna.0 && first_antenna.1 < second_antenna.1 {
         first_antinode.0 = first_antenna.0 + width_gap;
-        first_antinode.1 = first_antenna.0 - width_gap;
+        first_antinode.1 = first_antenna.1 - height_gap;
 
         second_antinode.0 = second_antenna.0 - width_gap;
         second_antinode.1 = second_antenna.1 + height_gap;
     } else if first_antenna.0 < second_antenna.0 && first_antenna.1 < second_antenna.1 {
         first_antinode.0 = first_antenna.0 - width_gap;
-        first_antinode.1 = first_antenna.0 - width_gap;
+        first_antinode.1 = first_antenna.1 - height_gap;
 
         second_antinode.0 = second_antenna.0 + width_gap;
         second_antinode.1 = second_antenna.1 + height_gap;
     }
 
-    if first_antinode.0 < grid.width && first_antinode.1 < grid.height && first_antinode.0 >= 0 && first_antinode.1 >= 0 {
+    if first_antinode.0 < grid.width
+        && first_antinode.1 < grid.height
+        && first_antinode.0 >= 0
+        && first_antinode.1 >= 0
+    {
         result.push(first_antinode);
     }
-    if second_antinode.0 < grid.width && second_antinode.1 < grid.height && second_antinode.0 >= 0 && second_antinode.1 >= 0 {
+    if second_antinode.0 < grid.width
+        && second_antinode.1 < grid.height
+        && second_antinode.0 >= 0
+        && second_antinode.1 >= 0
+    {
         result.push(second_antinode);
     }
     result
 }
 
-fn pair_two_antennas(update: &[(isize, isize)]) -> Vec<(usize, usize)> {
+fn pair_two_antennas(antenna: &[(isize, isize)]) -> Vec<(usize, usize)> {
     let mut result: Vec<(usize, usize)> = vec![];
-    for first_index in 0..(update.len() - 1) {
-        let mut second_index: usize = first_index + 1;
-        while second_index < update.len() {
-            result.push((first_index, second_index));
-            second_index += 1
+    for i in 0..(antenna.len()-1) {
+        for j in (i+1)..antenna.len() {
+            result.push((i, j))
         }
     }
     result
